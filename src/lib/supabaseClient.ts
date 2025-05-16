59dc49d34b5a9ab7f5702c9bb8803a48ae8be6f9
Luc1k1488 <boldarevsergei228@gmail.com>
2025-05-17 01:28:42 +0500

    Create supabaseClient.ts

---
+++ src/lib/supabaseClient.ts
@@ -0,0 +1,7 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://czwnegeanikobvdndnx.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN6d3duZWdlYW5pa29idmRuZG54Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc0MjQ4MTUsImV4cCI6MjA2MzAwMDgxNX0.ie8SNrBRKSlfez--tmrsMV4QgznpdxjYEnKTX59Yedc'
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+//added supabaseClient.ts
